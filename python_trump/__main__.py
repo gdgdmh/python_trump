8d6cacc5ebfddd588e4ac442a13bb80b024576f0
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-04-17 17:13:19 +0900

    不要コードを削除

--- python_trump/__main__.py
+++ python_trump/__main__.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 """エントリーポイント"""
-# from python_trump import trump
-# from python_trump import trump_deck
 
 
 def main():
     print('main')
-    # trump_test = trump.Trump(trump.Trump.SPADE, 1)
-    # trump_test.print_string()
-
-    # deck = trump_deck.TrumpDeck()
-    # deck.set_full_deck()
-
+    # test code
     list = range(1, 15)
     for num in list:
         print(str(num))
+
 
 if __name__ == '__main__':
     main()
