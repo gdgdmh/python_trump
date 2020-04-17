43f5d99996e7bd99fe9edc887f8fb7107b00119c
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-04-17 16:45:28 +0900

    デッキセット時の重複処理をリファクタリング

--- python_trump/trump_deck.py
+++ python_trump/trump_deck.py
@@ -14,20 +14,12 @@ class TrumpDeck:
         """デッキに54枚(ジョーカー2枚)のカードを設定する"""
         self.deck.append(trump.Trump(trump.Trump.JOKER, 1))
         self.deck.append(trump.Trump(trump.Trump.JOKER, 2))
-        for n in range(13):
-            self.deck.append(trump.Trump(trump.Trump.SPADE, n + 1))
-            self.deck.append(trump.Trump(trump.Trump.HEART, n + 1))
-            self.deck.append(trump.Trump(trump.Trump.DIAMOND, n + 1))
-            self.deck.append(trump.Trump(trump.Trump.CLUB, n + 1))
+        self.set_no_joker_deck()
 
     def set_one_joker_deck(self):
         """デッキに53枚(ジョーカー1枚)のカードを設定する"""
         self.deck.append(trump.Trump(trump.Trump.JOKER, 1))
-        for n in range(13):
-            self.deck.append(trump.Trump(trump.Trump.SPADE, n + 1))
-            self.deck.append(trump.Trump(trump.Trump.HEART, n + 1))
-            self.deck.append(trump.Trump(trump.Trump.DIAMOND, n + 1))
-            self.deck.append(trump.Trump(trump.Trump.CLUB, n + 1))
+        self.set_no_joker_deck()
 
     def set_no_joker_deck(self):
         """デッキに52枚のカードを設定する"""
