fe43458364a6bd253794630fb0ed9a9050e6e2cd
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-04-18 20:52:04 +0900

    手札クラスのコードを追加

---
+++ python_trump/trump_hand.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""トランプ手札クラス"""
+
+
+class TrumpHand:
+    """手札クラス"""
+
+    def __init__(self):
+        """コンストラクタ"""
+        pass
+
+    def add(self, trump):
+        """トランプを手札に加える"""
+        pass
+
+    def remove(self, index):
+        """手札からトランプを除く"""
+        pass
+
+    def index(self):
+        """手札からカードを取得する"""
+        pass
+
+    def size(self):
+        """手札の枚数"""
+        pass
+
+    def is_empty(self):
+        """手札が空か"""
+        pass
---
+++ tests/test_trump_hand.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""手札テストクラス"""
+from python_trump import old_maid
+
+
+def test_1_001():
+    pass
