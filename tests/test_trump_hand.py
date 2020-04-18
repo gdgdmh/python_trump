79959cd82c9958fd13eb037b2c2167f0d97d1970
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-04-19 00:08:45 +0900

    removeのテストを追加

--- tests/test_trump_hand.py
+++ tests/test_trump_hand.py
@@ -5,7 +5,7 @@ from python_trump import trump
 
 
 def test_add_001():
-    """トランプを手札に追加"""
+    """トランプを手札に追加チェック"""
     hand = trump_hand.TrumpHand()
     hand.add(trump.Trump(trump.Trump.JOKER, 1))
     assert hand.size() == 1
@@ -15,7 +15,7 @@ def test_add_001():
 
 
 def test_add_002():
-    """トランプを手札に追加(2枚)"""
+    """トランプを手札に追加チェック(2枚)"""
     hand = trump_hand.TrumpHand()
     hand.add(trump.Trump(trump.Trump.CLUB, 13))
     hand.add(trump.Trump(trump.Trump.DIAMOND, 5))
@@ -26,3 +26,22 @@ def test_add_002():
     t2 = hand.index(1)
     assert t2.get_suit() == trump.Trump.DIAMOND
     assert t2.get_number() == 5
+
+
+def test_remove_001():
+    """手札からトランプを削除チェック(1枚)"""
+    hand = trump_hand.TrumpHand()
+    hand.add(trump.Trump(trump.Trump.CLUB, 13))
+    hand.remove(0)
+    assert hand.size() == 0
+
+
+def test_remove_002():
+    """手札からトランプを削除チェック(2枚)"""
+    hand = trump_hand.TrumpHand()
+    hand.add(trump.Trump(trump.Trump.SPADE, 5))
+    hand.add(trump.Trump(trump.Trump.JOKER, 1))
+    hand.remove(0)
+    assert hand.size() == 1
+    hand.remove(0)
+    assert hand.size() == 0
