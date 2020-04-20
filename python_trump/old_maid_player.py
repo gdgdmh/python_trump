f2436cf6e5833a881a28b8b6b01c1671b94a1088
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-04-20 20:37:29 +0900

    プレイヤークラスに名前と手札を追加

--- python_trump/old_maid_player.py
+++ python_trump/old_maid_player.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 """ババ抜きプレイヤークラス"""
+from python_trump import trump_hand
 
 
 class OldMaidPlayer:
 
     def __init__(self):
         """コンストラクタ"""
-        pass
+        self.name = "p"
+        self.hand = trump_hand.TrumpHand()
+
+    def add_hand(self, card):
+        """カードを手札に加える"""
+        self.hand.add(card)
