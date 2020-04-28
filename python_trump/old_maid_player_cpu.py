4a99cba69c0c678946bb35e584be70f919df63fd
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-04-28 15:30:48 +0900

    プレイヤーCPUクラスを追加

---
+++ python_trump/old_maid_player_cpu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""ババ抜きプレイヤーCPUクラス"""
+from python_trump import old_maid_player
+
+
+class OldMaidPlayerCpu(old_maid_player.OldMaidPlayer):
+
+    def __init__(self, player_name='p'):
+        """コンストラクタ"""
+        super(OldMaidPlayerCpu, self).__init__(player_name)
+
+    def event_turn_start(self, trump_list):
+        """手番開始"""
+        pass
+
+    def event_turn_select(self, trump_list):
+        """手番カード選択"""
+        return None
---
+++ tests/test_old_maid_player_cpu.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""ババ抜きプレイヤーテストクラス"""
+from python_trump import old_maid_player_cpu
+from python_trump import old_maid_player
+from python_trump import trump
+
+
+def test_event_turn_start_001():
+    """手番開始"""
+    m = old_maid_player_cpu.OldMaidPlayerCpu()
+    c = old_maid_player.OldMaidPlayer()
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 1))
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 2))
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 3))
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 4))
+    m.event_turn_start(c.get_hand())
+
+
+def test_event_turn_select_001():
+    """手番カード選択"""
+    m = old_maid_player_cpu.OldMaidPlayerCpu()
+    c = old_maid_player.OldMaidPlayer()
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 1))
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 2))
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 3))
+    c.add_hand(trump.Trump(trump.Trump.SPADE, 4))
+    m.event_turn_select(c.get_hand())
