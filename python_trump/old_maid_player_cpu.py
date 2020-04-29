3ad6dad1bd74708fd403780b0bca306e8f0e949c
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-04-29 17:34:04 +0900

    乱数生成クラスを実装 CPUプレイヤーが出すカードのindexを返すようにした

--- python_trump/old_maid_player_cpu.py
+++ python_trump/old_maid_player_cpu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """ババ抜きプレイヤーCPUクラス."""
 from python_trump import old_maid_player
+from python_trump import system_random_int
 
 
 class OldMaidPlayerCpu(old_maid_player.OldMaidPlayer):
@@ -9,6 +10,7 @@ class OldMaidPlayerCpu(old_maid_player.OldMaidPlayer):
     def __init__(self, player_name='p'):
         """コンストラクタ."""
         super(OldMaidPlayerCpu, self).__init__(player_name)
+        self._random = system_random_int.SystemRandomInt()
 
     def event_turn_start(self, trump_list):
         """手番開始."""
@@ -16,4 +18,7 @@ class OldMaidPlayerCpu(old_maid_player.OldMaidPlayer):
 
     def event_turn_select(self, trump_list):
         """手番カード選択."""
-        return None
+        size = len(trump_list)
+        assert size == 0
+        tindex = self._random.get_range(0, size - 1)
+        return tindex
